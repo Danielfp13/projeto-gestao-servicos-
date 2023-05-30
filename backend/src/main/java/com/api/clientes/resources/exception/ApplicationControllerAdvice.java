0664c22270d13f8314057b3ceddc7ca7c629a66f
Daniel Pereira <daniel.si@outlook.com.br>
2023-05-30 13:48:46 -0300

    outras configurações 28

--- backend/src/main/java/com/api/clientes/resources/exception/ApplicationControllerAdvice.java
+++ backend/src/main/java/com/api/clientes/resources/exception/ApplicationControllerAdvice.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class AppliacationControllerAdvice {
+public class ApplicationControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
