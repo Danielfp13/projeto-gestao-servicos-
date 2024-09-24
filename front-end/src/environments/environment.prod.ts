38e14ba5472b2e2ab09a7da65d74d069d750b00c
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2024-09-24 08:12:09 -0300

    configurando na aws 2

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURLBase: 'http://18.220.185.202:8080',
+  apiURLBase: 'https://gestaoservicos.hopto.org:8080',
  // apiURLBase: 'https://projeto-gestao-servicos.onrender.com',
   obterTokenUrl: `/auth/signin`
 };
